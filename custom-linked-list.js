4b76b3dc1cf5e770fe0422c716806e8401e409a1
devIncogni <arinkumar10@outlook.com>
2025-05-19 21:19:34 +0530

    add head tail at pop contains find and toString functions

--- custom-linked-list.js
+++ custom-linked-list.js
@@ -28,13 +28,71 @@ class LinkedList {
     }
   }
 
-  size() {
+  get size() {
     let size = 0;
-    let currentNode = this.headNode;
+    let currentNode = this.head;
     while (currentNode.next !== null) {
       size += 1;
       currentNode = currentNode.next;
     }
     return size;
+  }
+
+  get head() {
+    return this.headNode;
+  }
+
+  get tail() {
+    return this.tailNode;
+  }
+
+  at(index) {
+    let currentNode = this.head;
+    if (index > this.size) {
+      return null;
+    }
+    for (let i = 0; i <= index; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  pop() {
+    let listSize = this.size;
+    if (listSize < 1) {
+      return;
+    }
+    this.at(listSize - 1).next = null;
+    this.tailNode = this.at(listSize - 1);
+  }
+
+  contains(value) {
+    for (let i = 0; i <= this.size; i++) {
+      if (String(this.at(i).value) === String(value)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  find(value) {
+    for (let i = 0; i <= this.size; i++) {
+      if (String(this.at(i).value) === String(value)) return i;
+      return null;
+    }
+  }
+
+  toString() {
+    if (this.head === null) {
+      return `null`;
+    }
+
+    let currentNode = this.head;
+    let listString = ``;
+
+    for (let i = 0; i <= this.size; i++) {
+      listString += `( ${currentNode.value} ) -> `;
+      currentNode = currentNode.next;
+    }
   }
 }
