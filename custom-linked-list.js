b4995d97059e087785014c9c4756a55d462f82e9
devIncogni <arinkumar10@outlook.com>
2025-05-19 20:17:31 +0530

    Wrote append and prepend functions

--- custom-linked-list.js
+++ custom-linked-list.js
@@ -1,7 +1,30 @@
 class Node {
-  constructor() {
-    this.value = null;
-    this.next = null;
+  constructor(value = null, next = null) {
+    this.value = value;
+    this.next = next;
   }
 }
 
+class LinkedList {
+  constructor() {
+    this.headNode = null;
+    this.tailNode = null;
+  }
+
+  prepend(value) {
+    this.headNode = new Node(value, this.headNode);
+    if (this.tailNode === null) {
+      this.tailNode = this.headNode;
+    }
+  }
+
+  append(value) {
+    if (this.headNode === null) {
+      this.tailNode = new Node(value);
+      this.headNode = this.tailNode;
+    } else {
+      this.tailNode.next = new Node(value);
+      this.tailNode = this.tailNode.next;
+    }
+  }
+}
